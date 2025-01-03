6752393d36e76dc47d16513cd824b3258d67112d
paranjothi iOS <32846534+Anilkumar18@users.noreply.github.com>
2025-01-03 17:28:12 +0530

    update xc framework

---
+++ Source/FootPrintsAISDK.xcframework/ios-arm64_x86_64-simulator/FootPrintsAISDK.framework/Headers/FootPrintsAISDK.h
@@ -0,0 +1,18 @@
+//
+//  FootPrintsAISDK.h
+//  FootPrintsAISDK
+//
+//  Created by Achu Anil's MacBook Pro on 30/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FootPrintsAISDK.
+FOUNDATION_EXPORT double FootPrintsAISDKVersionNumber;
+
+//! Project version string for FootPrintsAISDK.
+FOUNDATION_EXPORT const unsigned char FootPrintsAISDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FootPrintsAISDK/PublicHeader.h>
+
+
